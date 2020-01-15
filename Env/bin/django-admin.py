fe6c60e4eb69e4f6de254135a875aca245bef9b6
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2020-01-15 22:08:00 +0700

    First Commit

---
+++ Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/najmuddin/Documents/Lessons/Projects/Python/First Web/Env/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
