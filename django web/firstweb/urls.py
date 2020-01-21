b20747aee625a39b4c1ac6e4a9ea1c1402fecdd9
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2020-01-21 23:25:17 +0700

    using url in app

--- django web/firstweb/urls.py
+++ django web/firstweb/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views as v
-from blog import views as vb
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',v.index),
     path('about/',v.about),
-    path('blog/',vb.index)
+    path('blog/',include('blog.urls'))
 ]
